1368c3492a8b69a1094becce58a46bf6f0574680
Julien Zghendi <j.zghendi@hotmail.com>
2025-09-21 17:54:52 +0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pacifico } from "next/font/google";
 import "./globals.css";
 import ClientLoader from "./ClientLoader";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
